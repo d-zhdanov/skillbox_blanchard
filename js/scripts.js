71fa85d23300eed686f549e100e6c1038b9b6a1f
d-zhdanov <d.zhdanov@maestrochess.ru>
2021-09-08 14:47:12 +0700

    some things

--- js/scripts.js
+++ js/scripts.js
@@ -69,4 +69,31 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   };
 
+  var selector = document.querySelector("input[type='tel']");
+  var im = new Inputmask("+7 (999)-999-99-99");
+  im.mask(selector);
+
+  new JustValidate('.contacts__form', {
+    rules: {
+      name: {
+        required: true,
+        minLength: 2,
+        maxLength: 30
+      },
+
+      tel: {
+        required: true,
+        function: (name, value) => {
+          const phone = selector.inputmask.unmaskedvalue()
+          return Number(phone) && phone.length === 10
+        }
+      },
+
+    },
+
+    messages: {
+      name: 'Как вас зовут?',
+      tel: 'Укажите ваш телефон',
+    }
+  });
 });
